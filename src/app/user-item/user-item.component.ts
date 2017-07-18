fbde9f29b69b1ca79a7a8c77d186237ec22eccc5
arunsivan <arunsivan18.8@gmail.com>
2017-07-19 01:27:19 +0530

    commiy on 1.27 am

---
+++ src/app/user-item/user-item.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-item',
+  templateUrl: './user-item.component.html',
+  styleUrls: ['./user-item.component.css']
+})
+export class UserItemComponent implements OnInit {
+
+name:string;
+  constructor() { 
+     this.name="aruns";
+  
+  }
+
+  ngOnInit() {
+      this.name="arum";
+  }
+
+}
